603e027a4f5bb06245b900e9511f0de13718dc33
Guillaume Aldebert <galdebert@gmail.com>
2025-11-17 17:09:45 +0100

    internals/src/signUp

---
+++ internals/src/signUp.ts
@@ -0,0 +1,17 @@
+// COMMENTED BECAUSE aws-amplify/auth is not a dependency
+
+// import { signUp } from "aws-amplify/auth";
+
+// await signUp({
+//   username: "jdoe",
+//   password: "mysecurerandompassword#123",
+//   options: {
+//     userAttributes: {
+//       email: "me@domain.com",
+//       phone_number: "+12128601234", // E.164 number convention
+//       given_name: "Jane",
+//       family_name: "Doe",
+//       nickname: "Jane",
+//     },
+//   },
+// });
